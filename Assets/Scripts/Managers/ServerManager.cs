d415dba874bc1f876774e3efcdfa1c26eb43bec1
Mike <matthews.r.mike@gmail.com>
2022-10-17 00:18:16 +0200

    Added first server commands

--- Assets/Scripts/Managers/ServerManager.cs
+++ Assets/Scripts/Managers/ServerManager.cs
@@ -1,36 +1,77 @@
-// using PlayFab;
-// using PlayFab.ClientModels;
+using System;
+using System.Collections.Generic;
+using PlayFab;
+using PlayFab.ClientModels;
 using UnityEngine;
 
-/// <summary>Manages connexion to Playfab servers</summary>
-public class ServerManager : MonoBehaviour
-{
-	// TODO : Link that in with the rest of the game
+using static DifficultyManager;
+using static MapsManager;
 
+/// <summary>Manages connexion to Playfab servers</summary>
+public static class ServerManager
+{
 	// video tutorial link
 	// https://www.youtube.com/watch?v=DQWYMfZyMNU&list=PL1aAeF6bPTB4oP-Tejys3n8P8iXlj7uj-
 
 	// website link
 	// https://developer.playfab.com/en-US/BDD4E/dashboard
 
-	void Login()
-	{
-		// LoginWithCustomIDRequest request = new LoginWithCustomIDRequest()
-		// {
-		// CustomId = SystemInfo.deviceUniqueIdentifier,
-		// CreateAccount = true
-		// };
+	public static bool HasConnection => Application.internetReachability != NetworkReachability.NotReachable;
 
-		// PlayFabClientAPI.LoginWithCustomID(request, OnSuccess, OnError);
+	public static void Login()
+	{
+		if (!HasConnection)
+		{
+			Save.Data.firstGame = false;
+			return;
+		}
+
+		LoginWithCustomIDRequest request = new LoginWithCustomIDRequest()
+		{
+			CustomId = Save.Data.playerName,
+			CreateAccount = true
+		};
+
+		PlayFabClientAPI.LoginWithCustomID(
+			request,
+			result => Save.Data.firstGame = false,
+			error => Debug.LogError(error.GenerateErrorReport())
+		);
 	}
 
-	// void OnSuccess(LoginResult result)
-	// {
+	// TODO : Call this from "first game" panel
+	public static void IsNameValid(string name, Action OnSuccess, Action OnFail)
+	{
+		GetAccountInfoRequest request = new GetAccountInfoRequest() { Username = name };
+		PlayFabClientAPI.GetAccountInfo(request, result => OnSuccess(), error => OnFail());
+	}
 
-	// }
+	public static void SendScore(int completionTimeMil, MapSize size, Difficulty difficulty)
+	{
+		if (!HasConnection)
+		{
+			// TODO : store the statistics so that we can send them later ;)
+			return;
+		}
 
-	// void OnError(PlayFabError error)
+		string leaderboardName = "Score" + size.ToString() + "_" + difficulty;
+
+		UpdatePlayerStatisticsRequest request = new UpdatePlayerStatisticsRequest()
+		{
+			Statistics = new List<StatisticUpdate>(){ new StatisticUpdate() {
+				StatisticName = leaderboardName,
+				Value = completionTimeMil
+			}}
+		};
+
+		// TODO : Finish this
+		// PlayFabClientAPI.UpdatePlayerStatistics(request, OnSendScoreSuccess, OnSendScoreFail);
+	}
+
+	// static void OnSendScoreSuccess(UpdatePlayerStatisticsResult result) => Save.Data.firstGame = false;
+
+	// static void OnSendScoreFail(PlayFabError error)
 	// {
-	// error.GenerateErrorReport();
+	// Debug.LogError(error.GenerateErrorReport());
 	// }
 }
