0fe43d75b64c898b8f4bef3885244056ceb93a83
Mike <matthews.r.mike@gmail.com>
2022-11-02 20:57:52 +0100

    Add special sprites for completion popup

--- Assets/Scripts/UI/Popups/PopupMessage.cs
+++ Assets/Scripts/UI/Popups/PopupMessage.cs
@@ -4,8 +4,14 @@ using UnityEngine;
 /// <summary>Popup that displays a message</summary>
 public class PopupMessage : PopupBase
 {
+	[Header("Settings")]
+	public float rotationSpeed;
+
 	[Header("Scene references")]
 	public TMP_Text message;
+	public RectTransform specialBackground;
+
+	void Update() => specialBackground.Rotate(0, 0, -rotationSpeed * Time.deltaTime);
 
 	new void Awake() => base.Awake();
 
